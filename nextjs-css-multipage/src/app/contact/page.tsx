ff24b9cddebf3e8a3d6a586ac937534618aaef5b
Rubabfatima14 <rubab.faisal9093@habibschools.edu.pk>
2024-11-29 22:41:27 +0500

    Update page.tsx

--- nextjs-css-multipage/src/app/contact/page.tsx
+++ nextjs-css-multipage/src/app/contact/page.tsx
@@ -6,20 +6,13 @@ export default function Contact(){
     <div>
       <div className="main">
         <div className="about">
-   <h1 className="name" style={{fontSize:"50px",fontStyle:"italic"}} >Connect with Najaf Ali </h1>
+   <h1 className="name" style={{fontSize:"50px",fontStyle:"italic"}} >Connect with Rubab Fatima </h1>
 
-         <h1 className="intro"> <strong>Email:</strong> najafali32304@gmail.com</h1>
+         <h1 className="intro"> <strong>Email:</strong> rubab.fatima8888@gmail.com</h1>
           <h1 className="intro"> <strong>Address:</strong>Karachi Sindh, Pakistan</h1>
-          <h1 className="intro"> <strong>Phone:</strong> +923041729625</h1>
-          <h1 className="intro"> <strong>WhatsApp</strong> +923041729625</h1>
-          <h1 className="intro"> <strong>Github:</strong> najafali14</h1>
-          <h1 className="intro"> <strong>Discord:</strong> najafali14</h1>
-          <h1 className="intro"> <strong>LinkedIn:</strong> najafali14</h1>
-
-
-
-
-          </div>
+          <h1 className="intro"> <strong>Phone:</strong> +923452117948</h1>
+          <h1 className="intro"> <strong>WhatsApp</strong> +923452117948</h1>
+           </div>
 
         <div className="img" style={{marginLeft:"20%"}} >
       <Image src={hbg} width={"500"} height={"500"} alt="logo" className="himg" />
